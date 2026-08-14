25193857fea67117b8a6813308fbc1526eb8e639
Deepak Yadav <dspyadavdeepak01@gmail.com>
2026-08-14 18:10:27 +0530

    Basics & under the hood of python

---
+++ 01_Basics/chai.py
@@ -0,0 +1,10 @@
+from hello import chai_aur_code
+
+chai_aur_code("Chai aur code by hitesh choudhary")
+
+
+import hello
+from importlib import reload
+reload(hello)
+
+print(hello.one)
---
+++ 01_Basics/hello.py
@@ -0,0 +1,17 @@
+print("hello world!")
+
+def chai_aur_code(n):
+    print(n)
+
+chai_aur_code("Python with chai aur code")
+
+
+for char in "Deepak Yadav":
+    print(char)
+
+
+one = "Deepak Yadav"
+two = "Jyoti Yadav"
+three = "hitesh choudhary"
+
+ 
