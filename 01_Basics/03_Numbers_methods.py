910797331951c48c9e04c9488537fe92ffbd455f
Deepak Yadav <dspyadavdeepak01@gmail.com>
2026-08-21 20:57:16 +0530

    methods of numbeers

---
+++ 01_Basics/03_Numbers_methods.py
@@ -0,0 +1,66 @@
+# Number Conversion
+x = 10.75
+print(int(x))
+
+x = 10
+print(float(x))
+
+x = complex(5)
+print(x)
+x = complex(5, 3)
+print(x)
+
+
+x = 10.5678
+print(round(x))
+print(round(x, 2))
+print(round(x, 3))
+print(round(x, 4))
+print(round(x, 5))
+
+
+
+# abs() — Absolute Value
+print(abs(-10))
+print(abs(10))
+print(abs(-25.5))
+
+
+# pow()
+print(pow(2, 3))
+
+
+
+# min() और max()
+numbers = [10, 50, 20, 5, 30]
+print(min(numbers))
+print(max(numbers))
+
+# sum()
+numbers = [10, 20, 30, 40]
+print(sum(numbers))
+
+# type()
+print(type(True))
+print(type(1))
+print(type(0))
+print(type(1.2))
+print(type(-3.5))
+print(type(3+4j))
+
+
+a = 10
+b = 10.5
+c = "Hello"
+d = [1, 2, 3]
+e = (1, 2, 3)
+f = {1, 2, 3}
+g = {"name": "Deepak"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
